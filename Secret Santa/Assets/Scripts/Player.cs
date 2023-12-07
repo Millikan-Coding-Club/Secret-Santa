0e3821705b40e97c46d740c3177c408bded0c407
Steve <goldenskgaming@gmail.com>
2023-12-06 19:38:37 -0800

    Update 0.1
    Added Snow Tileset with Grid
    Organized and added files
    Created 4 directional Santa with moving and idle states

--- Secret Santa/Assets/Scripts/Player.cs
+++ Secret Santa/Assets/Scripts/Player.cs
@@ -7,20 +7,49 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Animator anim;
+    public float MovementSpeed;
+    private Vector2 MovementInput;
+
+
+    private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    // }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Animate();
     }
+
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized *  speed;
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+
+        MovementInput = new Vector2(Horizontal, Vertical);
+        rb.velocity = MovementInput * speed;
+        
+    }
+    private void Move() {
+    }
+    private void Animate() {
+        Debug.Log(MovementInput.x);
+        if (MovementInput != Vector2.zero) {
+            anim.SetFloat("MovementX", MovementInput.x);
+            anim.SetFloat("MovementY", MovementInput.y);
+            anim.SetBool("Idle", false);
+        }
+        else {
+            anim.SetBool("Idle", true);
+        }
     }
 }
