ecbd7b28f8bfd75a39134c7979ab6f8f95ba0099
Steve <goldenskgaming@gmail.com>
2023-12-13 20:30:32 -0800

    Added Gun Movement

--- Secret Santa/Assets/Scripts/Player.cs
+++ Secret Santa/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     private Animator anim;
     private Vector2 MovementInput;
+    public GameObject Shotgun;
 
 
     private void Awake() {
@@ -34,18 +35,41 @@ public class Player : MonoBehaviour
         float Horizontal = Input.GetAxisRaw("Horizontal");
         float Vertical = Input.GetAxisRaw("Vertical");
 
-        MovementInput = new Vector2(Horizontal, Vertical);
+        MovementInput = new Vector2(Horizontal, Vertical).normalized;
         rb.velocity = MovementInput * speed;
         
     }
     private void Move() {
     }
     private void Animate() {
-        Debug.Log(MovementInput.x);
         if (MovementInput != Vector2.zero) {
             anim.SetFloat("MovementX", MovementInput.x);
             anim.SetFloat("MovementY", MovementInput.y);
             anim.SetBool("Idle", false);
+            if (MovementInput.y == 0) { // left and right
+                Shotgun.transform.localPosition = new Vector3(0, -0.165f, 1);
+                Vector2 scale = Shotgun.transform.localScale;
+                // Shotgun.transform.rotation = Quaternion.identity;
+
+                if (MovementInput.x == -1) {
+
+                    scale.y = -.35f;
+                }
+                else {
+
+                    scale.y = .35f;
+                }
+                Shotgun.transform.localScale = scale;
+
+            }
+            else 
+             {
+                int z = 0;
+                if (MovementInput.y == -1) { // 
+                    z = 1;
+                }
+                Shotgun.transform.localPosition = new Vector3(0.23f, -.165f, z);
+            }
         }
         else {
             anim.SetBool("Idle", true);
